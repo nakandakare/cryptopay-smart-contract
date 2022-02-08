f84327b179d0bfa2d3a7d2bb5e9e09f494052a92
nakandakare <kevin.nakandakare@gmail.com>
2022-02-08 05:09:02 -0300

    smart contrac done

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+require('@nomiclabs/hardhat-waffle');
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  }
+}
